1520af2657c4214d560aec1248d7fd7599d846c6
joeshaw824 <57156322+joeshaw824@users.noreply.github.com>
2021-05-28 16:59:33 +0100

    Added section for reading in paternal SNP genotyping worksheets and collating the genotypes together.

--- scripts/ddpcr_snp_panel.R
+++ scripts/ddpcr_snp_panel.R
@@ -2,8 +2,8 @@
 ## ddPCR SNP panel analysis
 ## May 2021
 ## Joseph.Shaw@gosh.nhs.uk 
-## This script is for calculating the probability of the 
-## ddPCR SNP panel being uninformative for fetal fraction.
+## These scripts deal with the ddPCR SNP panel used for 
+## determining the fetal fraction of cfDNA.
 ## This panel was originally described by Camunas-Soler et
 ## al (2018) (PMID: 29097507)
 #############################################################
@@ -13,6 +13,13 @@
 #############################################################
 
 library(tidyverse)
+
+## Set working directory
+setwd("W:/MolecularGenetics/NIPD translational data/NIPD Droplet Digital PCR/ddPCR_R_Analysis/ddpcr_nipd")
+
+# Load resources
+controls <- readr::read_csv("resources/controls.csv")
+ddpcr_target_panel <- readr::read_csv("resources/ddpcr_target_panel.csv") 
 
 snp_panel <- read.csv("W:/MolecularGenetics/NIPD translational data/NIPD Droplet Digital PCR/ddPCR SNP Panel/Camunas_Soler_panel_47_GnomAD_frequencies.csv")
 
@@ -171,4 +178,40 @@ ggplot(snp_calc, aes(x = GOSH_ID_ds, y = frequency_B))+
   ylim(0, 0.5)+
   labs(x = "SNPs", y = "GnomAD minor allele frequency", title = "GnomAD minor allele frequencies of Camunas-Soler et al (2018) ddPCR SNP panel")+
   theme_bw()
+
+#############################################################
+# Collating parental SNP genotypes
+#############################################################
+
+dataPath <- "data/ddPCR_SNP_genotyping/"
+
+ddpcr_files <- list.files(dataPath)
+
+#Empty data frame
+SNP_data <- data.frame()
+
+# Read and collate each worksheet csv
+for (dataFile in ddpcr_files){
+  tmp_dat <- read_csv(paste0(dataPath,dataFile), col_names = TRUE)
+  SNP_data <-rbind(SNP_data, tmp_dat)
+  rm(tmp_dat)
+}
+
+SNP_data_table <- SNP_data %>%
+  select(Sample, Target, TargetType, Concentration) %>%
+  left_join(ddpcr_target_panel %>%
+              select(Target, Assay), by = "Target") %>%
+  # Convert the concentration column to a numeric and convert "no call" to zero
+  mutate(Copies_per_ul = as.integer(ifelse(Concentration == "No Call", 0, Concentration))) %>%
+  # Add fluorophores
+  mutate(Label = case_when(
+    TargetType == "Ch1Unknown" ~ "FAM",
+    TargetType == "Ch2Unknown" ~ "VIC")) %>%
+  select(!c(Target, Concentration, TargetType)) %>%
+  pivot_wider(
+    id_cols = Assay,
+    names_from = c(Sample, Label),
+    values_from = Copies_per_ul)
+
+
 
